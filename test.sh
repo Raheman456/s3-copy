0247d5f4b7f09d5cf8a6843cdf2420541d5e2cef
Raheman456 <90036934+Raheman456@users.noreply.github.com>
2023-09-29 20:26:03 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,13 +6,16 @@ AWS_SECRET_ACCESS_KEY="DqY6ZEA0FtnBpj42t8/GzXOw9qNqhvSW77qvVTUb"
 aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
 aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
 
-if [ $# -ne 2 ]; then
-  echo "Usage: $0 <source_filename> <destination_filename>"
-  exit 1
-fi
+#if [ $# -ne 2 ]; then
+ # echo "Usage: $0 <source_filename> <destination_filename>"
+  #exit 1
+#fi
 
-SOURCE_FILENAME="$1"
-DEST_FILENAME="$2"
+#SOURCE_FILENAME="$1"
+#DEST_FILENAME="$2"
+
+SOURCE_FILENAME="$SOURCE_FILE"
+DEST_FILENAME="$DEST_FILE"
 
 SOURCE_S3_PATH="s3://devops0507/sample/sample1/sample2/$SOURCE_FILENAME"
 DEST_S3_PATH="s3://devops0507/test/test1/test2/$DEST_FILENAME"
